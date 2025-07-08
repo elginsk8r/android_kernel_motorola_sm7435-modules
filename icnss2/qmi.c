46cec08f49ae2110bd00c9f5da3ad289d42b3c06
Zhiye Li <lizy90@motorola.com>
2025-07-08 11:41:25 +0800

    wlan: different sku select the corresponding the bdf
    wlan:
    use the device name to select the correct bdf
    
    Change-Id: Ia6565cb147c7cd7e6f505a244ac39706c63b33ba
    Signed-off-by: Zhiye Li <lizy90@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3456833
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Submit-Approved: Jira Key

--- icnss2/qmi.c
+++ icnss2/qmi.c
@@ -216,7 +216,7 @@ static int selectFileNameByProduct(struct icnss_priv *plat_priv, char *filename,
 
 	num = num_of_products(products_list);
 	for (i = 0; i < num; i++) {
-		if (strncmp(device_ptr, (products_list+i)->hw_device, strlen(device_ptr)) == 0) {
+		if (strcmp(device_ptr, (products_list+i)->hw_device) == 0) {
 			if(strcmp(radio_ptr, (products_list+i)->hw_radio) == 0 || strcmp((products_list+i)->hw_radio, "all") == 0) {
 				if(ICNSS_BDF_ELF == bdf_type) {
 					if(0x400c1211 == plat_priv->soc_id)  //GF chip
