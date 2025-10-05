46f45c31e43b80d85b084f3d72ab38e4ba207a6e
Nick Reuter <nreuter85@gmail.com>
2025-10-05 15:01:15 -0500

    Revert "kernel warning -sleeping function called from invalid context"
    This reverts commit 42dcdfcda98cfab3ff103b1d3ebfdf57f7a9c92c.
    
    Change-Id: If0dac26d72e20b8bff6b66f1797a3a2c7279c28f

--- motorola/include/linux/touchscreen_mmi.h
+++ motorola/include/linux/touchscreen_mmi.h
@@ -503,7 +503,6 @@ struct ts_mmi_dev {
 	enum ts_mmi_pm_mode	pm_mode;
 
 	struct delayed_work	work;
-	struct delayed_work	ps_work;
 	struct kfifo		cmd_pipe;
 
 	struct notifier_block	freq_nb;
@@ -512,8 +511,6 @@ struct ts_mmi_dev {
 	struct work_struct	ps_notify_work;
 	struct notifier_block	ps_notif;
 	bool			ps_is_present;
-	bool			present;
-	struct			power_supply *psy;
 
 	struct notifier_block	fps_notif;
 	bool is_fps_registered;	/* FPS notif registration might be delayed */
